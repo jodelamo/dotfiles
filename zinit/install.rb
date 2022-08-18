b19cef36fbb15ea97da84dd411e8f4981c0d6bab
Joacim de la Motte <joacim.delamotte@gmail.com>
2022-08-18 14:28:36 +0200

    chore: remove zinit

--- zinit/install.rb
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env ruby
-
-system('sh -c "$(curl -fsSL https://raw.githubusercontent.com/zdharma/zinit/master/doc/install.sh)"')
--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,3 +1,6 @@
+fpath=("$(brew --prefix)/share/zsh/site-functions" $fpath)
+
 # Enable custom prompt themes
-autoload -U promptinit && promptinit
+autoload -U promptinit; promptinit
+
 prompt pure
