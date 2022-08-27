4f717aef2892b70ef61ee52e973f16e3fa2e75a9
Joacim de la Motte <joacim.delamotte@gmail.com>
2022-08-27 09:25:08 +0200

    chore: remove unused path in $MANPATH

--- system/path.zsh
+++ system/path.zsh
@@ -1,2 +1,2 @@
-export PATH="/usr/local/bin:./bin:$HOME/.rbenv/shims:/usr/local/sbin:$ZSH/bin:$PATH:$GOPATH/bin"
-export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
+export PATH="/usr/local/bin:./bin:$HOME/.rbenv/shims:/usr/local/sbin:$ZSH/bin:$GOPATH/bin:$PATH"
+export MANPATH="/usr/local/man:/usr/local/git/man:$MANPATH"
