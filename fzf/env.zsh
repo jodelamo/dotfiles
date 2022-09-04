e7a2ffc18bdd7d3ee80dd7367fac24b8a3779350
Joacim de la Motte <joacim.delamotte@gmail.com>
2022-09-04 21:07:12 +0200

    chore(rg): replace ag/ack with ripgrep

--- fzf/env.zsh
+++ fzf/env.zsh
@@ -1,3 +1,3 @@
-export FZF_DEFAULT_COMMAND='ag -l -g ""'
+export FZF_DEFAULT_COMMAND="rg --files --no-ignore-vcs --hidden"
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="$FZF_DEFAULT_COMMAND"
