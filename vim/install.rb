e6d7bc1782a1130bcaf831e080c9c0b6f899c545
Joacim de la Motte <joacim.delamotte@gmail.com>
2023-01-19 21:19:37 +0100

    chore(vim): replace paq with packer

--- vim/install.rb
+++ vim/install.rb
@@ -3,11 +3,12 @@
 require "fileutils"
 
 # rubocop:disable Layout/LineLength
-# Install paq-nvim
-system("git clone --depth=1 https://github.com/savq/paq-nvim.git \"${XDG_DATA_HOME:-$HOME/.local/share}\"/nvim/site/pack/paqs/start/paq-nvim")
+# Install packer.nvim
+system("git clone --depth=1 https://github.com/wbthomason/packer.nvim \"${XDG_DATA_HOME:-$HOME/.local/share}\"/nvim/site/pack/packer/start/packer.nvim")
 # rubocop:enable Layout/LineLength
 
 config_dir = FileUtils.mkdir_p("#{Dir.home}/.config/nvim")
 src_file = File.join(__dir__, "init.lua")
 dest_file = File.join(config_dir, "init.lua")
-FileUtils.ln_s(src_file, dest_file)
+
+FileUtils.ln_s(src_file, dest_file) unless File.exist?(dest_file)
