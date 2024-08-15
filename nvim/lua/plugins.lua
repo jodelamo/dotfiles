1eb72f3b964502ebcabb7390f3d46ca51e8654b5
Joacim de la Motte <joacim.delamotte@gmail.com>
2024-08-15 14:57:49 +0200

    chore(nvim): remove nvim-lint

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -158,17 +158,6 @@ require("lazy").setup({
 	},
 
 	{
-		"mfussenegger/nvim-lint",
-		config = function()
-			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
-				callback = function()
-					require("lint").try_lint()
-				end,
-			})
-		end,
-	},
-
-	{
 		"williamboman/mason-lspconfig.nvim",
 		dependencies = {
 			"neovim/nvim-lspconfig",
