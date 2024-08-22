e619b4254504d48ef5a095d59ab609f72183eef1
Joacim de la Motte <joacim.delamotte@gmail.com>
2024-08-22 21:52:39 +0200

    chore(python): remove unused parameter

--- python/aliases.zsh
+++ python/aliases.zsh
@@ -1,1 +1,1 @@
-alias pu="pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
+alias pu="pip list --outdated | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U"
