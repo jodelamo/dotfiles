ed161c23a44f4e1459ab4176180daaa979312198
Joacim de la Motte <joacim.delamotte@gmail.com>
2024-10-28 10:09:23 +0100

    chore(nvim): show hidden files with ripgrep

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -107,6 +107,24 @@ require("lazy").setup({
 			"nvim-lua/plenary.nvim",
 		},
 		config = function()
+			require("telescope").setup({
+				pickers = {
+					find_files = {
+						hidden = true,
+					},
+				},
+				live_grep = {
+					additional_args = function()
+						return { "--hidden", "--glob", "!**/.git/*" }
+					end,
+				},
+				grep_string = {
+					additional_args = function()
+						return { "--hidden", "--glob", "!**/.git/*" }
+					end,
+				},
+			})
+
 			local builtin = require("telescope.builtin")
 			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
