24c09a57cd92adc92a99db2b225f57c84e457674
Joacim de la Motte <joacim.delamotte@gmail.com>
2025-01-07 10:21:43 +0100

    chore(nvim): replace completion with blink.cmp

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -274,62 +274,6 @@ require("lazy").setup({
 	},
 
 	{
-		"hrsh7th/cmp-buffer",
-	},
-
-	{
-		"hrsh7th/cmp-cmdline",
-	},
-
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-
-	{
-		"hrsh7th/cmp-path",
-	},
-
-	{
-		"hrsh7th/nvim-cmp",
-		init = function()
-			vim.opt.completeopt = { "menu", "menuone", "noselect" }
-		end,
-		config = function()
-			local cmp = require("cmp")
-
-			cmp.setup({
-				window = {
-					completion = cmp.config.window.bordered(),
-					documentation = cmp.config.window.bordered(),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-b>"] = cmp.mapping.scroll_docs(-4),
-					["<C-f>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-e>"] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "nvim_lua" },
-				}, {
-					{ name = "buffer" },
-					{ name = "path" },
-				}),
-			})
-
-			cmp.setup.cmdline(":", {
-				mapping = cmp.mapping.preset.cmdline(),
-				sources = cmp.config.sources({
-					{ name = "path" },
-				}, {
-					{ name = "cmdline" },
-				}),
-			})
-		end,
-	},
-
-	{
 		"jghauser/mkdir.nvim",
 	},
 
@@ -398,6 +342,22 @@ require("lazy").setup({
 			"mfussenegger/nvim-dap",
 			"nvim-neotest/nvim-nio",
 		},
+	},
+
+	{
+		"saghen/blink.cmp",
+		dependencies = "rafamadriz/friendly-snippets",
+		version = "*",
+		opts = {
+			keymap = { preset = "default" },
+			appearance = {
+				nerd_font_variant = "mono",
+			},
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
+		},
+		opts_extend = { "sources.default" },
 	},
 
 	{
