86800c9743042e4ae1324e10ed7de91450f63278
Joacim de la Motte <joacim.delamotte@gmail.com>
2025-02-27 09:45:41 +0100

    chore: fix indentation

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,15 +15,15 @@ awk -F '=' -v section="$section" '
     print $1, $2;
   }
 ' "$ini_file" | while read -r dest source; do
-if [ -e "$source" ]; then
-  # Ensure the destination directory exists
-  dest_dir=$(dirname "$dest")
-  mkdir -p "$dest_dir"
+  if [ -e "$source" ]; then
+    # Ensure the destination directory exists
+    dest_dir=$(dirname "$dest")
+    mkdir -p "$dest_dir"
 
-  # Create symlink
-  echo "🔗 Create symlink: $source -> $dest"
-  ln -sfn "$source" "$dest"
-else
-  echo "Warning! Source does not exist: $source"
-fi
+    # Create symlink
+    echo "🔗 Create symlink: $source -> $dest"
+    ln -sfn "$source" "$dest"
+  else
+    echo "Warning! Source does not exist: $source"
+  fi
 done
