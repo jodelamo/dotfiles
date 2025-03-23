001248aab9208c2911b230ca1cec629b25988085
Joacim de la Motte <joacim.delamotte@gmail.com>
2025-03-23 09:32:44 +0100

    refactor(zsh): replace pure-prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,6 +1,29 @@
-# fpath=("$(brew --prefix)/share/zsh/site-functions" $fpath)
+autoload -Uz vcs_info
 
-# Enable custom prompt themes
-autoload -U promptinit; promptinit
+zstyle ':vcs_info:*' enable git
+zstyle ':vcs_info:*' check-for-changes true
+zstyle ':vcs_info:*' unstagedstr '!'
+zstyle ':vcs_info:*' stagedstr '+'
+zstyle ':vcs_info:*' formats '[%b]%u%c'
+# zstyle ':vcs_info:*+*:*' debug true
 
-prompt pure
+setopt PROMPT_SUBST
+
+zstyle ':vcs_info:git*+set-message:*' hooks git-untracked
+
++vi-git-untracked() {
+  if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
+    git status --porcelain | grep -q '^?? ' 2> /dev/null ; then
+    hook_com[staged]+='?'
+  fi
+}
+
+precmd() {
+  vcs_info
+
+  if [[ -z ${vcs_info_msg_0_} ]]; then
+    PROMPT="%B%F{cyan}%~%f%b %# "
+  else
+    PROMPT="%B%F{cyan}%~%f%b %F{magenta}${vcs_info_msg_0_}%f %# "
+  fi
+}
