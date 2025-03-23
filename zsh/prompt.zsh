0042c93c4365667419105402670ed025ed762da5
Joacim de la Motte <joacim.delamotte@gmail.com>
2025-03-23 11:32:15 +0100

    chore(zsh): change prompt symbols

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -12,6 +12,7 @@ zstyle ':vcs_info:git*+set-message:*' hooks git-st git-untracked
 # zstyle ':vcs_info:*+*:*' debug true
 
 # Show "?" if we have untracked files
+# Ref: https://github.com/zsh-users/zsh/blob/master/Misc/vcs_info-examples#L163
 +vi-git-untracked() {
   if [[ $(git rev-parse --is-inside-work-tree 2> /dev/null) == 'true' ]] && \
     git status --porcelain | grep -q '^?? ' 2> /dev/null ; then
@@ -20,6 +21,7 @@ zstyle ':vcs_info:git*+set-message:*' hooks git-st git-untracked
 }
 
 # Show number of commits ahead-of or behind
+# Ref: https://github.com/zsh-users/zsh/blob/master/Misc/vcs_info-examples#L180
 +vi-git-st() {
   local ahead behind
   local -a gitstatus
@@ -34,8 +36,8 @@ zstyle ':vcs_info:git*+set-message:*' hooks git-st git-untracked
   ahead=${ahead_and_behind[1]}
   behind=${ahead_and_behind[2]}
 
-  (( $ahead )) && gitstatus+=( "+${ahead}" )
-  (( $behind )) && gitstatus+=( "-${behind}" )
+  (( $ahead )) && gitstatus+=( " ↑" )
+  (( $behind )) && gitstatus+=( "↓" )
 
   hook_com[misc]+=${(j:/:)gitstatus}
 }
@@ -46,6 +48,6 @@ precmd() {
   if [[ -z ${vcs_info_msg_0_} ]]; then
     PROMPT="%B%F{cyan}%~%f%b %# "
   else
-    PROMPT="%B%F{cyan}%~%f%b %F{magenta}${vcs_info_msg_0_}%f %# "
+    PROMPT="%B%F{cyan}%~%f%b %F{yellow}${vcs_info_msg_0_}%f %# "
   fi
 }
