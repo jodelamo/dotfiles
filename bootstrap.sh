8cbc0ef721514983eeeea2be6826c0eda6601fae
Joacim de la Motte <joacim.delamotte@gmail.com>
2025-05-07 07:13:39 +0200

    chore(brew): use homebrew on linux as well

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,6 +3,12 @@
 ini_file="config.ini"
 section="links"
 
+# Install Homebrew if not installed
+if ! command -v brew &> /dev/null; then
+  echo "Homebrew not found. Installing..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
 if [ "$(uname)" = "Darwin" ]; then
   ./bin/macos
 elif [ "$(uname)" = "Linux" ]; then
