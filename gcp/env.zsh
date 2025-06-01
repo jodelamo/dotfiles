5d7c57785acd52ac0d185e91fba2948408cb4294
Joacim de la Motte <joacim.delamotte@gmail.com>
2025-06-01 16:46:45 +0200

    chore(gcp): check if gcloud exists

--- gcp/env.zsh
+++ gcp/env.zsh
@@ -1,6 +1,6 @@
 export CLOUDSDK_PYTHON=$HOME/.pyenv/shims/python
 
-if command -v brew >/dev/null 2>&1; then
+if command -v gcloud >/dev/null 2>&1; then
   source "$(brew --prefix)/share/google-cloud-sdk/path.zsh.inc"
   source "$(brew --prefix)/share/google-cloud-sdk/completion.zsh.inc"
 fi
