9f7c3ce22c3c08a933847489f50259e26e194dcd
Joacim de la Motte <joacim.delamotte@gmail.com>
2025-09-01 07:15:09 +0200

    chore(nvim): remove codecompanion extension

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -4,26 +4,15 @@ require("lazy").setup({
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-treesitter/nvim-treesitter",
-			"ravitemer/mcphub.nvim",
 		},
 		opts = {
-			extensions = {
-				mcphub = {
-					callback = "mcphub.extensions.codecompanion",
-					opts = {
-						make_vars = true,
-						make_slash_commands = true,
-						show_result_in_chat = true,
-					},
-				},
-			},
 			strategies = {
 				chat = {
 					adapter = "copilot",
 					tools = {
 						opts = {
 							default_tools = {
-								"buffer",
+								"files",
 							},
 						},
 					},
