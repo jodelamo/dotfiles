754ef59fee586964b72ef2dbfef04ed5dabdeb75
Joacim de la Motte <joacim.delamotte@gmail.com>
2025-09-11 10:46:55 +0200

    chore(nvim): update keymaps

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -1,3 +1,3 @@
 -- Don't lose selection when shifting left or right
-vim.keymap.set("x", "<", "<gv")
-vim.keymap.set("x", ">", ">gv")
+vim.keymap.set({ "x", "v" }, "<", "<gv", { silent = true, desc = "Shift left and reselect" })
+vim.keymap.set({ "x", "v" }, ">", ">gv", { silent = true, desc = "Shift right and reselect" })
