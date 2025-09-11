2f224b2592e322d7f3c5975836c6e47d8bcf8e90
Joacim de la Motte <joacim.delamotte@gmail.com>
2025-09-11 10:43:30 +0200

    chore(nvim): clean up various options

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -7,23 +7,17 @@ vim.o.updatetime = 250
 -- colorscheme
 vim.opt.termguicolors = true -- true color support
 vim.opt.background = "dark"
--- vim.cmd("colorscheme tokyonight")
-
--- leave the past behind
-vim.opt.compatible = false
 
 -- make copy operations work with the clipboard
-vim.opt.clipboard = "unnamed"
+vim.opt.clipboard = "unnamedplus"
 
 -- indentation
 vim.opt.cindent = true
-vim.opt.autoindent = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.shiftround = true -- always indent/outdent to the nearest tabstop
 vim.opt.expandtab = true -- use spaces instead of tabs
-vim.opt.smarttab = true -- use tabs at the start of a line, spaces elsewhere
 vim.opt.wrap = false
 
 -- auto-reload files changed on disk
@@ -35,8 +29,7 @@ vim.opt.backup = false
 vim.opt.swapfile = false
 
 -- persistent undo
-vim.opt.undodir = "/tmp"
-vim.opt.hidden = true
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
 vim.opt.undofile = true
 vim.opt.undolevels = 1000
 vim.opt.undoreload = 10000
@@ -44,19 +37,9 @@ vim.opt.undoreload = 10000
 -- guides
 vim.opt.number = true
 vim.opt.colorcolumn = "80"
-vim.opt.history = 200
-vim.opt.ruler = true
 
 -- search
-vim.opt.hlsearch = true -- highlight all results
-vim.opt.incsearch = true -- but do highlight as you type your search.
-vim.opt.ignorecase = true -- make searches case-insensitive...
-vim.opt.smartcase = true -- ... unless they contain at least one capital letter
-vim.opt.gdefault = true -- have :s///g flag by default on"
-
--- split windows right and below
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+vim.opt.gdefault = true -- have :s///g flag on by default
 
 -- substitution
 vim.opt.inccommand = "nosplit" -- preview effects of substitution
