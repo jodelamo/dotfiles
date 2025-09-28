3637c209ea96ae31503760816ba09c652c5e4e1d
Joacim de la Motte <joacim.delamotte@gmail.com>
2025-09-28 13:31:17 +0200

    chore(nvim): fix stylelint lsp

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -1,29 +1,29 @@
 return {
-	{
-		"williamboman/mason-lspconfig.nvim",
-		dependencies = {
-			{ "mason-org/mason.nvim", opts = {} },
-			"neovim/nvim-lspconfig",
-		},
-		opts = {
-			automatic_enable = true,
-			ensure_installed = {
-				"biome",
-				"html",
-				"lua_ls",
-				"pyright",
-				"ruby_lsp",
-				"stylelint-lsp",
-				"terraformls",
-				"ts_ls",
-				"v_analyzer",
-				"yamlls",
-			},
-		},
-	},
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = {
+      { "mason-org/mason.nvim", opts = {} },
+      "neovim/nvim-lspconfig",
+    },
+    opts = {
+      automatic_enable = true,
+      ensure_installed = {
+        "biome",
+        "html",
+        "lua_ls",
+        "pyright",
+        "ruby_lsp",
+        "stylelint_lsp",
+        "terraformls",
+        "ts_ls",
+        "v_analyzer",
+        "yamlls",
+      },
+    },
+  },
 
-	{
-		-- Auto-install conform formatters
-		"zapling/mason-conform.nvim",
-	},
+  {
+    -- Auto-install conform formatters
+    "zapling/mason-conform.nvim",
+  },
 }
