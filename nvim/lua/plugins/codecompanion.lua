91632622878aac0864677e34309857986b51145a
Joacim de la Motte <joacim.delamotte@gmail.com>
2025-11-11 06:20:34 +0100

    chore(nvim): codecompanion clean up

--- nvim/lua/plugins/codecompanion.lua
+++ nvim/lua/plugins/codecompanion.lua
@@ -43,26 +43,19 @@ return {
 				mcphub = {
 					callback = "mcphub.extensions.codecompanion",
 					opts = {
-						-- Make individual tools (@server__tool) and server groups (@server) from MCP servers
 						make_tools = true,
-						-- Show individual tools in chat completion (when make_tools=true)
 						show_server_tools_in_chat = true,
-						-- Add mcp__ prefix (e.g `@mcp__github`, `@mcp__neovim__list_issues`)
 						add_mcp_prefix_to_tool_names = false,
-						-- Show tool results directly in chat buffer
 						show_result_in_chat = true,
-						-- function(tool_name:string, tool: CodeCompanion.Agent.Tool) : string Function to format tool names to show in the chat buffer
 						format_tool = nil,
-						-- Convert MCP resources to #variables for prompts
 						make_vars = true,
-						-- Add MCP prompts as /slash commands
 						make_slash_commands = true,
 					},
 				},
 			},
 		},
 		init = function()
-			-- Expand 'cc' into 'CodeCompanion' in the command line
+			vim.cmd([[cab ccc CodeCompanionChat]])
 			vim.cmd([[cab cc CodeCompanion #{buffer}]])
 			vim.cmd([[cab ca CodeCompanionActions]])
 
