18e95c7d5a5490a47b3c727acfe8bb17a0341163
Joacim de la Motte <joacim.delamotte@gmail.com>
2026-05-11 08:04:51 +0200

    feat(rust): install fixed version

--- rust/install.sh
+++ rust/install.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+version="1.95.0"
+
 if ! command -v rustup > /dev/null 2>&1; then
   curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 else
   rustup update
 fi
+
+rustup toolchain install "$version"
+rustup default "$version"
 
 packages="tree-sitter-cli"
 
