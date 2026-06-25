6a828ad4dc89d0c44b13cabb00461da79583dd73
Joacim de la Motte <joacim.delamotte@gmail.com>
2026-06-25 07:18:00 +0200

    fix: correct POSIX compliance and typos across scripts
    - luarocks/install.sh: rewrite to POSIX sh (was using zsh syntax
      but called with sh), fix broken array iteration
    - homebrew/Brewfile: remove trailing space in font-iosevka cask name,
      rename rg to ripgrep (correct formula name)
    - bin/macos: replace non-POSIX &> with > /dev/null 2>&1
    - bin/linux: add --no-install-recommends to apt install

--- luarocks/install.sh
+++ luarocks/install.sh
@@ -1,9 +1,7 @@
-#!/usr/bin/env zsh
+#!/bin/sh
 
-local -a packages=(
-  "tiktoken_core"
-)
+packages="tiktoken_core"
 
-for p in "$packages"; do
+for p in $packages; do
   luarocks install "$p"
 done
