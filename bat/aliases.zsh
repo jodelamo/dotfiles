1f67ae9931fabc8dea1a33567d2c7eda901bd01a
Joacim de la Motte <joacim.delamotte@gmail.com>
2026-07-05 13:23:24 +0200

    feat(bat): support batcat binary name from apt

--- bat/aliases.zsh
+++ bat/aliases.zsh
@@ -1,1 +1,5 @@
-alias cat="bat --style=plain --paging=never"
+if (( $+commands[bat] )); then
+  alias cat="bat --style=plain --paging=never"
+elif (( $+commands[batcat] )); then
+  alias cat="batcat --style=plain --paging=never"
+fi
