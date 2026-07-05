3fd18f6fd82891d1a893474759e297a4810d7973
Joacim de la Motte <joacim.delamotte@gmail.com>
2026-07-05 10:41:09 +0200

    chore(node): remove aliases.zsh

--- node/aliases.zsh
+++
@@ -1,7 +0,0 @@
-# Update npm packages to latest version available
-nug () {
-  for package in $(npm -g outdated --parseable --depth=0 | cut -d: -f2)
-  do
-      npm -g install "$package"
-  done
-}
